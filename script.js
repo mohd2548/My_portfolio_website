79863a3a50210746ad30881cf1322ed863ce72d7
mohd2548 <mohdsahila132@gmail.com>
2025-02-23 01:32:09 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let currentIndex = 0;
 const slides = document.querySelector(".slides");
 const totalSlides = document.querySelectorAll(".slide").length;
@@ -16,5 +15,40 @@ function prevSlide() {
     currentIndex = (currentIndex - 1 + totalSlides) % totalSlides; // Loop back if at 1st
     updateSlide();
 }
+
+let Education = document.querySelector(".Education")
+let General = document.querySelector(".General")
+let Personal = document.querySelector(".Personal")
+
+
+Education.addEventListener("click", () => {
+    Education.style.backgroundColor = "red";
+    General.style.backgroundColor = "";
+    Personal.style.backgroundColor = "";
+    Education.style.padding = "3px"
+    let paragraph = document.querySelector("#about p");
+    paragraph.innerText = "Education is Education";
+
+})
+
+General.addEventListener("click", () => {
+    Education.style.backgroundColor = "";
+    General.style.backgroundColor = "red";
+    Personal.style.backgroundColor = "";
+    General.style.padding = "3px"
+    let paragraph = document.querySelector("#about p");
+    paragraph.innerText = "genral is general";
+})
+
+Personal.addEventListener("click", () => {
+    Education.style.backgroundColor = "";
+    General.style.backgroundColor = "";
+    Personal.style.backgroundColor = "red";
+    Personal.style.padding = "3px"
+    let paragraph = document.querySelector("#about p");
+    paragraph.innerText = "personal is personal";
+})
+
+
 
 
